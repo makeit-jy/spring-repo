1f42d9546d0738aeb13529fb01e5beae16e098e9
makeit-jy <makeit_jy@naver.com>
2024-06-08 15:35:27 +0900

    first commit

---
+++ src/test/java/com/jungle/springbootmyboard/SpringbootMyBoardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jungle.springbootmyboard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootMyBoardApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
